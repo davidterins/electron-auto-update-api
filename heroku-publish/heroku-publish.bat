24a12079fa44452ed9e2b751604c631e32a26586
DTee <david.terins@telia.com>
2021-10-17 22:54:01 +0200

    Updated configs

--- heroku-publish/heroku-publish.bat
+++ heroku-publish/heroku-publish.bat
@@ -1,5 +1,5 @@
 dotnet publish ../ElectronAutoUpdateApi/ElectronAutoUpdateApi.csproj -c release -o ../ElectronAutoUpdateApi/bin/publish
-docker build -t nameOfYourApp ../ElectronAutoUpdateApi/bin/
-docker tag nameOfYourApp registry.heroku.com/nameOfYourApp/web
-docker push registry.heroku.com/nameOfYourApp/web
-heroku container:release web -a nameOfYourApp
+docker build -t logspace-auto-update-api ../ElectronAutoUpdateApi/bin/
+docker tag logspace-auto-update-api registry.heroku.com/logspace-auto-update-api/web
+docker push registry.heroku.com/logspace-auto-update-api/web
+heroku container:release web -a logspace-auto-update-api
